7ef642483bd6b170dcd4c95a67e89bcfdb5a9aec
Arvindkaranje <98689570+Arvindkaranje@users.noreply.github.com>
2022-01-31 12:37:05 -0500

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -3,4 +3,11 @@ if a%2==0:
    print("its a prime)
    else:
        print("its not a prime)
-       
+             
+a=4
+if a==4:
+    print("four")
+    if a==2:
+        print("two")
+else:
+     print("no match")
