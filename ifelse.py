8b987b1e37bd6f8723d55614090261e9323739dd
Arvindkaranje <98689570+Arvindkaranje@users.noreply.github.com>
2022-01-31 12:41:06 -0500

    elifadded.py

--- ifelse.py
+++ ifelse.py
@@ -11,3 +11,12 @@ if a==4:
         print("two")
 else:
      print("no match")
+
+             a=1
+             if a==1:
+               print("one")
+             elif a==2:
+               print("two")
+             else:
+               print("out of range")
+             
